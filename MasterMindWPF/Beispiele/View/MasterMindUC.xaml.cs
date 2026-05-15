a8fac28d817ec56fce579e52ce7cfe178c82d228
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-05-15 11:08:55 +0200

    *

--- MasterMindWPF/Beispiele/View/MasterMindUC.xaml.cs
+++ MasterMindWPF/Beispiele/View/MasterMindUC.xaml.cs
@@ -45,6 +45,8 @@
             this.SelectR0C4.FillColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_availableColors[3]));
             this.SelectR0C5.FillColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_availableColors[4]));
             this.SelectR0C6.FillColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString(_availableColors[5]));
+
+            this.DisableAllPlayerColors();
         }
 
         private void OnSelectColor(object args)
@@ -63,7 +65,9 @@
                 {
                     Color = color,
                     Row = row,
-                    Col = col
+                    Col = col,
+                    PlayerWin = false,
+                    LineChecked = false
                 };
 
                 if (_playerColors.Any(pc => pc.Row == row && pc.Col == col && pc.Color == color) == false)
@@ -89,6 +93,45 @@
             }
         }
 
+        private void DisableAllPlayerColors()
+        {
+            this.PlayerR1C1.FillColor = Brushes.LightGray;
+            this.PlayerR1C1.IsEnabled = false;
+            this.PlayerR1C2.FillColor = Brushes.LightGray;
+            this.PlayerR1C2.IsEnabled = false;
+            this.PlayerR1C3.FillColor = Brushes.LightGray;
+            this.PlayerR1C3.IsEnabled = false;
+            this.PlayerR1C4.FillColor = Brushes.LightGray;
+            this.PlayerR1C4.IsEnabled = false;
+
+            this.PlayerR2C1.FillColor = Brushes.LightGray;
+            this.PlayerR2C1.IsEnabled = false;
+            this.PlayerR2C2.FillColor = Brushes.LightGray;
+            this.PlayerR2C2.IsEnabled = false;
+            this.PlayerR2C3.FillColor = Brushes.LightGray;
+            this.PlayerR2C3.IsEnabled = false;
+            this.PlayerR2C4.FillColor = Brushes.LightGray;
+            this.PlayerR2C4.IsEnabled = false;
+
+            this.PlayerR3C1.FillColor = Brushes.LightGray;
+            this.PlayerR3C1.IsEnabled = false;
+            this.PlayerR3C2.FillColor = Brushes.LightGray;
+            this.PlayerR3C2.IsEnabled = false;
+            this.PlayerR3C3.FillColor = Brushes.LightGray;
+            this.PlayerR3C3.IsEnabled = false;
+            this.PlayerR3C4.FillColor = Brushes.LightGray;
+            this.PlayerR3C4.IsEnabled = false;
+
+            this.PlayerR4C1.FillColor = Brushes.LightGray;
+            this.PlayerR4C1.IsEnabled = false;
+            this.PlayerR4C2.FillColor = Brushes.LightGray;
+            this.PlayerR4C2.IsEnabled = false;
+            this.PlayerR4C3.FillColor = Brushes.LightGray;
+            this.PlayerR4C3.IsEnabled = false;
+            this.PlayerR4C4.FillColor = Brushes.LightGray;
+            this.PlayerR4C4.IsEnabled = false;
+        }
+
         private sealed class PlayerColor
         {
             public Brush Color { get; set; }
@@ -96,6 +139,10 @@
             public int Row { get; set; }
 
             public int Col { get; set; }
+
+            public bool LineChecked { get; set; }
+
+            public bool PlayerWin { get; set; }
         }
     }
 }
